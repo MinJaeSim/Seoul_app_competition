cd752aa6a8f7d75738e236477df53ab2219b7617
MinJaeSim <mj9757@gmail.com>
2018-09-23 15:06:16 +0900

    리소스 추가 및 기본 플로우 프래그먼트 생성

--- app/src/main/java/com/example/mj975/woder_woman/MainActivity.java
+++ app/src/main/java/com/example/mj975/woder_woman/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.mj975.woder_woman;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mj975.woder_woman.fragment.CleanMapFragment;
+import com.example.mj975.woder_woman.fragment.MainFragment;
+import com.example.mj975.woder_woman.fragment.PersonFragment;
+import com.example.mj975.woder_woman.fragment.ReportFragment;
+import com.example.mj975.woder_woman.fragment.ServiceFragment;
+
+public class MainActivity extends BaseActivity {
+
+    private FragmentManager fm;
+    private BottomNavigationView bottomNavigation;
+    private long pressedTime;
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fm = getSupportFragmentManager();
+        Fragment f = fm.findFragmentById(R.id.fragment_container);
+
+        if (f == null) {
+            f = new MainFragment();
+            fm.beginTransaction().add(R.id.fragment_container, f).commit();
+        }
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new MainFragment();
+                    case R.id.action_clean:
+                        fragment = new CleanMapFragment();
+                    case R.id.action_report:
+                        fragment = new ReportFragment();
+                    case R.id.action_relieved:
+                        fragment = new ServiceFragment();
+                    case R.id.action_person:
+                        fragment = new PersonFragment();
+                }
+
+                fm.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                return false;
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed() {
+
+        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+            super.onBackPressed();
+            return;
+        }
+        if (pressedTime == 0) {
+            Toast.makeText(MainActivity.this, getString(R.string.string_close_warning), Toast.LENGTH_LONG).show();
+            pressedTime = System.currentTimeMillis();
+        } else {
+            int seconds = (int) (System.currentTimeMillis() - pressedTime);
+
+            if (seconds > 2000) {
+                Toast.makeText(MainActivity.this, getString(R.string.string_close_warning), Toast.LENGTH_LONG).show();
+                pressedTime = 0;
+            } else {
+                super.onBackPressed();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/mj975/woder_woman/fragment/CleanMapFragment.java
@@ -0,0 +1,22 @@
+package com.example.mj975.woder_woman.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mj975.woder_woman.R;
+
+public class CleanMapFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main,container,false);
+
+        return v;
+    }
+}
